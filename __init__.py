dc8dd19e2a10f18e264a168923f461f3c474af66
Michael Scott Cuthbert <cuthbert@mit.edu>
2016-04-19 13:37:25 -0400

    Change last of name

--- __init__.py
+++ __init__.py
@@ -1,20 +1,18 @@
 # -*- coding: utf-8 -*-
 #------------------------------------------------------------------------------
 # Name:         __init__.py
-# Purpose:      BBBalk -- A toolkit for computational baseball analysis 
+# Purpose:      Daseki -- A toolkit for computational baseball analysis 
 #
 # Authors:      Michael Scott Cuthbert
 #
-# Copyright:    Copyright © 2014-15 Michael Scott Cuthbert / cuthbertLab
+# Copyright:    Copyright © 2014-16 Michael Scott Cuthbert / cuthbertLab
 # License:      BSD, see license.txt
 #------------------------------------------------------------------------------
 '''
-BBBalk is a toolkit for computational baseball analysis, developed by
+Daseki is a toolkit for computational baseball analysis, developed by
 Michael Scott Cuthbert (Associate Prof. MIT).
 
-The title comes from baseball, obviously, and balk, as in what you should
-expect traditional baseball heads to do when you present the results you
-get from these objects.
+Copyright 2014-16 Michael Scott Cuthbert / cuthbertLab
 '''
 __all__ = ['ext', 
            'retro', 
