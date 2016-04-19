09a6ef62668bc1820d703c19033e8b8ffc290c9b
Michael Scott Cuthbert <cuthbert@mit.edu>
2016-04-19 13:41:22 -0400

    Update and rename exceptionsBB.py to exceptionsDS.py

--- exceptionsBB.py
+++
@@ -1,9 +0,0 @@
-'''
-All files need to be able to import here
-'''
-
-class BBBalkException(Exception):
-    pass
-
-class RetrosheetException(BBBalkException):
-    pass
---
+++ exceptionsDS.py
@@ -0,0 +1,9 @@
+'''
+All files need to be able to import this file, so do not put any imports here.
+'''
+
+class DasekiException(Exception):
+    pass
+
+class RetrosheetException(DasekiException):
+    pass
